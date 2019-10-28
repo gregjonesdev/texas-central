1fe360de38db04e0b8b97487698cf26537270918
Greg Jones <gmjones@gmail.com>
2019-10-28 15:11:39 -0500

    flex the form

--- src/components/app/TicketForm.tsx
+++ src/components/app/TicketForm.tsx
@@ -11,6 +11,45 @@ export const TicketForm = () =>
       <h3>Select Your Journey</h3>
     </div>
     <div className="card-body">
-      Fill out a form!
+      <div className="ticketform">
+        <div className="dropdown ticketselect">
+          <button
+            className="btn btn-outline-secondary dropdown-toggle"
+            data-toggle="dropdown">
+            Round Trip
+            <span className="caret"></span>
+          </button>
+          <div className="dropdown-menu">
+            <button className="btn btn-default btn-block">
+              One Way
+            </button>
+          </div>
+        </div>
+        <div className="dropdown ticketselect">
+          <button
+            className="btn btn-outline-secondary dropdown-toggle"
+            data-toggle="dropdown">
+            Round Trip
+            <span className="caret"></span>
+          </button>
+          <div className="dropdown-menu">
+            <button className="btn btn-default btn-block">
+              One Way
+            </button>
+          </div>
+        </div>  <div className="dropdown ticketselect">
+            <button
+              className="btn btn-outline-secondary dropdown-toggle"
+              data-toggle="dropdown">
+              Round Trip
+              <span className="caret"></span>
+            </button>
+            <div className="dropdown-menu">
+              <button className="btn btn-default btn-block">
+                One Way
+              </button>
+            </div>
+          </div>
+      </div>
     </div>
   </div>
